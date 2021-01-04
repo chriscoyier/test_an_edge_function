3e390a4b1142c51d64381cb04a533f1c6af4a08d
Chris Coyier <chriscoyier@gmail.com>
2021-01-04 15:59:38 -0800

    edge handler

---
+++ edge-handlers/addContent.js
@@ -0,0 +1,35 @@
+export function onRequest(event) {
+  if (event.request.url.pathname === "/secret") {
+    event.replaceResponse(async () => {
+      async function fetchContentJSON() {
+        const response = await fetch(
+          `https://css-tricks.com/wp-json/wp/v2/posts`
+        );
+        const data = await response.json();
+        return data;
+      }
+
+      let response;
+      let post;
+
+      fetchContentJSON().then((data) => {
+        post = data[0];
+      });
+
+      response = new Response(
+        `
+        <h1>${post.title}</h1>
+        ${post.excerpt.rendered}
+        `,
+        {
+          headers: {
+            "content-type": "text/html",
+          },
+          status: 200,
+        }
+      );
+
+      return response;
+    });
+  }
+}
