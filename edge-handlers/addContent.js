b5bbd66c123640587fb5ee510d8cfec5b77480a8
Chris Coyier <chriscoyier@gmail.com>
2021-01-18 17:28:45 -0800

    working version

--- edge-handlers/addContent.js
+++ edge-handlers/addContent.js
@@ -1,35 +1,40 @@
 export function onRequest(event) {
-  if (event.request.url.pathname === "/") {
-    event.replaceResponse(async () => {
-      async function fetchContentJSON() {
-        const response = await fetch(
-          `https://css-tricks.com/wp-json/wp/v2/posts`
-        );
-        const data = await response.json();
-        return data;
-      }
+  event.replaceResponse(async () => {
+    async function fetchContentJSON() {
+      const response = await fetch(
+        `https://css-tricks.com/wp-json/wp/v2/posts`
+      );
+      const data = await response.json();
+      return data;
+    }
 
-      let response;
-      let post;
+    let response;
+    let post;
 
-      await fetchContentJSON().then((data) => {
-        post = data[0];
-      });
+    await fetchContentJSON().then((data) => {
+      post = data[0];
+    });
 
-      response = new Response(
-        `
-        <h1>${post.title}</h1>
+    console.log(post);
+
+    response = new Response(
+      `<!doctype html>
+        <head>
+          <meta charset="UTF-8">
+        </head>
+        <h1>
+          <a href="${post.link}">${post.title.rendered}</a>
+        </h1>
         ${post.excerpt.rendered}
         `,
-        {
-          headers: {
-            "content-type": "text/html",
-          },
-          status: 200,
-        }
-      );
+      {
+        headers: {
+          "content-type": "text/html",
+        },
+        status: 200,
+      }
+    );
 
-      return response;
-    });
-  }
+    return response;
+  });
 }
